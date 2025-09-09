8588e1c385ab6f41f233a7330dbec4f2c9ead62b
DarkSorrow <darksorrow.slade@gmail.com>
2025-09-09 12:24:20 +0200

    fix: gitignore

---
+++ plugin/build/index.js
@@ -0,0 +1,7 @@
+const withRnFileinfo = (config) => {
+    // This plugin doesn't need to modify any native configurations
+    // The module should be autolinked automatically by React Native
+    // and discovered by Expo through expo-module.config.json
+    return config;
+};
+export default withRnFileinfo;
